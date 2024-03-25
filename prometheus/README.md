eb2df78fcc3147b3a0903cacc67b91d471bf3f4a
tengtaox <tetaciao@gmail.com>
2024-03-25 14:55:17 +0800

    upadte prometheus

---
+++ prometheus/README.md
@@ -0,0 +1,3 @@
+What needs to be changed:
+Job address in yml
+The address of the mount file in the install_job.sh file
--- prometheus/install_docker.sh
+++ prometheus/install_docker.sh
@@ -1,5 +1,5 @@
-docker run -d --name prometheus -p 9090:9090 -v prometheus.yml:/etc/prometheus/prometheus.yml prom/prometheus
+docker run -d --name prometheus -p 9090:9090 -v /root/install_sh/prometheus/prometheus.yml:/etc/prometheus/prometheus.yml prom/prometheus
 
-docker run -d --name node_exporter -p 9100:9100 --net="host" prom/node-exporter
+docker run -d --name node_exporter -p 9100:9100 prom/node-exporter
 
 
